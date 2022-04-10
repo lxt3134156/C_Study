5f1795facdf2fb16a37cf64de4ae8202f972498b
xiaolai <3134156@qq.com>
2022-04-10 14:29:35 +0800

    添加项目文件。

---
+++ add.c
@@ -0,0 +1,6 @@
+//#include "add.h"
+//
+////int g_val = 2022;
+//int add(int x, int y) {
+//	return x + y;
+//}
---
+++ study.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+//extern int g_val;
+//int main() {
+//	printf(" %d ", g_val);
+//	return 0;
+//}
+
+//int main(){
+//	int a = 10, b = 20;
+//	int sum = add(a, b);
+//	printf("%d",sum);
+//	return 0;
+//}
+//int main(){
+//	int a = 10;
+//	int* pa = &a;
+//	*pa = 20;
+//	printf("%d ",a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", sizeof(int*));
+//	printf("%d\n", sizeof(char*));
+//	printf("%d\n", sizeof(double*));
+//	printf("%d\n", sizeof(long*));
+//	return 0;
+//}
+
+//int Max(int x , int y){
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d",&a,&b);
+//	int m = Max(a, b);
+//	printf("%d\n",m);
+//	return 0;
+//}
+//int sum(int a) {
+//	int c = 0;
+//	static int b = 3;
+//	c += 1;
+//	b += 2;
+//	return (a + b + c);
+//}
+//int main() {
+//	int a = 2;
+//	for (int i = 0; i < 5; i++) {
+//		printf(" %d ", sum(a));
+//	}
+//}
+
+//2022/4/9
